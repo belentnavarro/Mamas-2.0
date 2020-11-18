8675661b4b7448d3290d20bd8300b090139d25a4
Belén Trujillo <belentnavarro@gmail.com>
2020-11-18 09:38:31 +0100

    Conexión BDD añadida.

---
+++ Model/GestionBDD.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of GestionBDD
+ *
+ * @author belentnavarro
+ */
+// Includes y requires
+require_once '../Auxiliar/Constants.php';
+
+class GestionBDD {
+    public static $conexion;
+    
+    public static function conectarBDD(){
+        self::$conexion = new mysqli("localhost", Constants::$USER, Constants::$PASSWORD, Constants::$DDBB);
+        
+        if(mysqli_connect_errno(self::$conexion)){
+            print "Fallo al conectar a MySQL: " . mysqli_connect_error();
+        } else {
+            print "Conexión realizada con éxito.";
+        }
+    }
+    
+    public static function cerrarBDD(){
+        self::$conexion->close();
+    }
+}
+
