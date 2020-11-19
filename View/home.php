59cb1613c028ee8a01028371f8624fc8ec5227f6
Belén Trujillo <belentnavarro@gmail.com>
2020-11-19 19:41:32 +0100

    Comienzo del home.

--- View/home.php
+++ View/home.php
@@ -26,7 +26,7 @@ and open the template in the editor.
 
         <!--Favicon-->
         <link rel="icon" type="image/png" href="../Img/logo/favicon-birrete.png">
-        
+
         <title>Mamas 2.0</title>
     </head>
     <body>
@@ -41,131 +41,145 @@ and open the template in the editor.
         // Recupero el rol del usuario
         $userRol = $_SESSION['userRol'];
         ?>
-        
-        
-<!--        <div class="container d-flex">
-            <div class="col-3">
-                 Sidebar 
-            <div id="sidebar "class="side-nav fixed bg--o-dark">
-                <ul class="custom-scrollbar ps lit-unstyled">
-                     Logo 
-                    <li class="text-center">
-                        <img src="Img/logo/birrete_1.png" alt="logo">
-                    </li>
-                    <li>
-                         Links de la sidebar 
-                        <ul class="collapsible collapsible-accordion">
-                            <?php 
-                            if($_SESSION['userRol'] == 'administrador'){
-                                ?>
-                            <li class="collapsible-header waves-effect">
-                                <a href="crud_admin_usuarios.php" class="waves-effect">
-                                    Gestor de Usuarios
-                                </a>
-                            </li>
-                            <?php
-                            }
-                            ?>
-                            <?php 
-                            if($_SESSION['userRol'] == 'administrador' || $_SESSION['userRol'] == 'profesor'){
-                                ?>
-                            <li>
-                                <a class="collapsible-header waves-effect">
-                                    Gestor de Exámenes <i class="fa fa-angle-down"></i>
-                                </a>
-                                <div class="collapsible-body">
-                                    <ul>
-                                        <li>
-                                            <a href="" class="waves-effect">
-                                                Crear examen
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="" class="waves-effect">
-                                                Exámenes
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="" class="waves-effect">
-                                                Corregir examen
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <?php
-                            }
-                            ?>
-                            <?php 
-                            if($_SESSION['userRol'] == 'usuario'){
-                                ?>
-                            <li>
-                                <a class="collapsible-header waves-effect">
-                                    Exámenes <i class="fa fa-angle-down"></i>
-                                </a>
-                                <div class="collapsible-body">
-                                    <ul>
-                                        <li>
-                                            <a href="" class="waves-effect">
-                                                Realizar examen
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="" class="waves-effect">
-                                                Exámenes activos
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="" class="waves-effect">
-                                                Exámenes realizados
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="" class="waves-effect">
-                                                Notas
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <?php
-                            }
-                            ?>
-                    </li>
-                </ul>
-            </div>
-            </div>
-            <div class="col-9">
-                Cabecera 
-        <header>
-            Navbar 
-            <nav class="navbar navbar-expand-lg bg--o-dark fixed-top scrolling-navbar double-nav" style="padding-left: unset;">
-                 SideNav botón para abrir el sidebar 
-                <div class="float-left">
-                    <a href="#" data-activates="slide-out" class="button-collapse">
-                        <i class="fa fa-bars"></i>
-                    </a> 
-                    <h1 class="display-3 font-weight-bolder text-light">mamas 2.0</h1>
-                </div>
-                 Link editar perfil 
-                <ul class="nav navbar-nav nav-flex-icons ml-auto lit-unstyled">
-                    <li class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle waves-effect" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                            <i class="fa fa-user"></i>
-                            <span class="clearfix d-none d-sm-inline-block text-light">Perfil</span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right show" aria-labelledby="navbarDropdownMenuLink">
-                            <a href="editar_perfil.php" class="dropdown-item waves-effect waves-light text-light">
-                                Mi perfil
+
+        <div class="container-fluid d-flex">
+            <!-- Sidebar -->
+            <div class="sidebar active pt-3 bg--o-dark">
+                <!-- Logo -->
+                <img class="ml-5" src="../Img/logo/birrete_1.png" alt="logo" width="100"/>
+                <ul class="list-unstyled pt-5">
+                    <!-- Links del sidebar -->
+                    <?php
+                    if ($_SESSION['userRol'] == 2) {
+                        ?>
+                        <li class="waves-effect text-light font-weight-bold mt-2">
+                            <a href="crud_admin_usuarios.php" class="h4">
+                                <svg class="bi ml-2" width="22" height="22" fill="currentColor">
+                                <use xlink:href="../Icons/bootstrap-icons.svg#people-fill"/>
+                                </svg>
+                                Gestor de Usuarios
                             </a>
-                        </div>                        
+                        </li>
+                        <?php
+                    }
+                    ?>
+                    <?php
+                    if ($_SESSION['userRol'] == 2 || $_SESSION['userRol'] == 1) {
+                        ?>
+                        <li class="waves-effect text-light font-weight-bold mt-2">
+                            <p class="h4">
+                                <svg class="bi ml-2" width="22" height="22" fill="currentColor">
+                                <use xlink:href="../Icons/bootstrap-icons.svg#journal-bookmark-fill"/>
+                                </svg>
+                                Gestor de Exámenes
+                                <svg class="bi mr-2" width="15" height="15" fill="currentColor">
+                                <use xlink:href="../Icons/bootstrap-icons.svg#arrow-down-circle-fill"/>
+                                </svg>
+                            </p>
+                            <ul>
+                                <li>
+                                    <a href="" class="waves-effect">
+                                        Crear examen
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" class="waves-effect">
+                                        Exámenes
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" class="waves-effect">
+                                        Corregir examen
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <?php
+                    }
+                    ?>
+                    <?php
+                    if ($_SESSION['userRol'] == 0) {
+                        ?>
+                        <li class="waves-effect mt-2">
+                            <p class="h4 text-light font-weight-bold">
+                                <svg class="bi ml-2" width="22" height="22" fill="currentColor">
+                                <use xlink:href="../Icons/bootstrap-icons.svg#journal-text"/>
+                                </svg>
+                                Exámenes
+                                <svg class="bi mr-2" width="15" height="15" fill="currentColor">
+                                <use xlink:href="../Icons/bootstrap-icons.svg#arrow-down-circle-fill"/>
+                                </svg>
+                            </p>
+                            <ul class="list-unstyled pl-5">
+                                <li class="waves-effect">
+                                    <a href="" class="p-1 text--g-light font-weight-bold">
+                                        Realizar exámenes
+                                    </a>
+                                </li>
+                                <li class="waves-effect">
+                                    <a href="" class="p-1 text--g-light font-weight-bold">
+                                        Exámenes activos
+                                    </a>
+                                </li>
+                                <li class="waves-effect">
+                                    <a href="" class="p-1 text--g-light font-weight-bold">
+                                        Exámenes realizados
+                                    </a>
+                                </li>
+                                <li class="waves-effect">
+                                    <a href="" class="p-1 text--g-light font-weight-bold">
+                                        Notas
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <?php
+                    }
+                    ?>
+                    <li class="waves-effect mt-2">
+                        <a href="../Contoller/controller.php?cerrar=1" class="h4 text-light font-weight-bold">
+                            <svg class="bi ml-2" width="22" height="22" fill="currentColor">
+                            <use xlink:href="../Icons/bootstrap-icons.svg#arrow-right-circle"/>
+                            </svg>
+                            Cerrar sesión
+                        </a>
                     </li>
                 </ul>
-            </nav>
-        </header>
             </div>
-        </div>-->
-        
+            <div id="content">
+                
+            </div>
+
+        </div>
+<!--                <header>
+                    Navbar 
+                    <nav class="navbar navbar-expand-lg bg--o-dark fixed-top scrolling-navbar double-nav" style="padding-left: unset;">
+                         SideNav botón para abrir el sidebar 
+                        <div class="float-left">
+                            <a href="#" data-activates="slide-out" class="button-collapse">
+                                <i class="fa fa-bars"></i>
+                            </a> 
+                            <h1 class="display-3 font-weight-bolder text-light">mamas 2.0</h1>
+                        </div>
+                         Link editar perfil 
+                        <ul class="nav navbar-nav nav-flex-icons ml-auto lit-unstyled">
+                            <li class="nav-item dropdown">
+                                <a href="#" class="nav-link dropdown-toggle waves-effect" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                    <i class="fa fa-user"></i>
+                                    <span class="clearfix d-none d-sm-inline-block text-light">Perfil</span>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right show" aria-labelledby="navbarDropdownMenuLink">
+                                    <a href="editar_perfil.php" class="dropdown-item waves-effect waves-light text-light">
+                                        Mi perfil
+                                    </a>
+                                </div>                        
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+                    </div>
+                </div>-->
+
         <!-- jQuery -->
         <script type="text/javascript" src="../Js/jquery.min.js"></script>
 
