337e7271ce5b5e53b85755656ca5065cf4771a12
Belén Trujillo <belentnavarro@gmail.com>
2020-11-27 10:16:32 +0100

    Crear examen funcional, vistas para añadir preguntas y examen creado

--- View/crud_add_questions_exam.php
+++ View/crud_add_questions_exam.php
@@ -29,7 +29,7 @@ and open the template in the editor.
         <!--Favicon-->
         <link rel="icon" type="image/png" href="../Img/logo/favicon-birrete.png">
 
-        <title>Crear examen</title>
+        <title>Añadir preguntas al examen</title>
     </head>
     <body>
         <?php
@@ -37,9 +37,9 @@ and open the template in the editor.
         require_once '../Model/Person.php';
         require_once '../Model/PersonDAO.php';
         require_once '../Model/Question.php';
-        //require_once '../Model/QuestionText.php';
-        //require_once '../Model/QuestionNumber.php';
-        //require_once '../Model/QuestionWritten.php';
+        require_once '../Model/QuestionText.php';
+        require_once '../Model/QuestionNumber.php';
+        require_once '../Model/QuestionWritten.php';
         //require_once '../Model/QuestionTextDAO.php';
         //require_once '../Model/QuestionNumberDAO.php';
         //require_once '../Model/QuestionWrittenDAO.php';
@@ -227,7 +227,70 @@ and open the template in the editor.
                         <div class="col-11">
                             <div class="card mb-0 shadow">
                                 <div class="card-header font-weight-bold text-white bg--o-light display-4 text-center">
-                                    Crear un examen
+                                    Preguntas añadidas
+                                </div>
+                                <div class="card-body">
+                                    <div class="row border-bottom font-weight-bolder mb-4 pb-0">
+                                        <div class="col">
+                                            Título
+                                        </div>
+                                        <div class="col">
+                                            Descripción
+                                        </div>
+                                        <div class="col">
+                                            Fecha de inicio
+                                        </div>
+                                        <div class="col">
+                                            Fecha fin
+                                        </div>
+                                        <div class="col">
+                                            Asignatura
+                                        </div>
+                                        <div class="col">
+                                            Acciones
+                                        </div>
+                                    </div> 
+                                    <form action="../Controller/controller_create_exam.php" method="POST" name="create_new_exam">
+                                        <div class="row">
+                                            <div class="col mb-2 align-items-start">
+                                                <input type="text" id="tittle" name="tittle" class="form-control" placeholder="Título" required>
+                                            </div>
+                                            <div class="col mb-2 align-items-center">
+                                                <textarea id="description" name="description" rows="6" cols="10" class="form-control" placeholder="Descripción del examen" required></textarea>
+                                            </div>
+                                            <div class="col mb-2 align-items-start">
+                                                <input type="date" id="startsAt" name="startsAt" class="form-control"/>
+                                            </div>
+                                            <div class="col mb-2 align-items-start">
+                                                <input type="date" id="endsAt" name="endsAt" class="form-control"/>
+                                            </div>
+                                            <div class="col mb-2 align-items-start">
+                                                <input type="text" id="subject" name="subject" class="form-control" placeholder="Asignatura" required>
+                                            </div>
+                                            <div class="col">
+                                                Acciones
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-4 col-md-4 offset-4 mt-4">
+                                                <button type="submit" class="btn btn--g-medium w-100 mt-0 font-weight-bold" name="create_exam" value="create_exam">
+                                                    Crear examen
+                                                    <svg class="bi ml-2" width="22" height="22" fill="currentColor">
+                                                    <use xlink:href="../Icons/bootstrap-icons.svg#plus-square-fill"/>
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center align-items-center">
+                        <div class="col-11">
+                            <div class="card mb-0 shadow">
+                                <div class="card-header font-weight-bold text-white bg--o-light display-4 text-center">
+                                    Lista de preguntas
                                 </div>
                                 <div class="card-body">
                                     <div class="row border-bottom font-weight-bolder mb-4 pb-0">
@@ -256,10 +319,10 @@ and open the template in the editor.
                                                 <textarea id="description" name="description" rows="6" cols="10" class="form-control" placeholder="Descripción del examen" required></textarea>
                                             </div>
                                             <div class="col mb-2 align-items-start">
-                                                <input type="date" id="startsAt" name="startsAt" class="form-control" required/>
+                                                <input type="date" id="startsAt" name="startsAt" class="form-control"/>
                                             </div>
                                             <div class="col mb-2 align-items-start">
-                                                <input type="date" id="endsAt" name="endsAt" class="form-control" required/>
+                                                <input type="date" id="endsAt" name="endsAt" class="form-control"/>
                                             </div>
                                             <div class="col mb-2 align-items-start">
                                                 <input type="text" id="subject" name="subject" class="form-control" placeholder="Asignatura" required>
@@ -308,6 +371,7 @@ and open the template in the editor.
         <script type="text/javascript" src="../Js/app.js"></script>
     </body>
 </html>
+
 
 
 
